826747e2c567a9559ac2a86acb7fe517cfab76a9
Ivan_Nemkovich <inemkovich@gmail.com>
2019-12-08 16:24:31 +0300

    First commit to upload alien_invasion on GitHub

---
+++ alien_invasion.py
@@ -0,0 +1,31 @@
+import pygame
+from pygame.sprite import Group
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+	"""Game init and create display obj"""
+	pygame.init()
+	ai_settings = Settings()
+	screen = pygame.display.set_mode(
+		(ai_settings.screen_width, ai_settings.screen_height))
+	pygame.display.set_caption("Alien Invasion")
+	
+	# Create ship
+	ship = Ship(ai_settings, screen)
+	# Create group for bullets
+	bullets = Group()
+	
+	# run game
+	while True:
+		# Mouse and keyboard detect
+		gf.check_events(ai_settings, screen, ship, bullets)
+		ship.update()
+		bullets.update()
+		gf.update_screen(ai_settings, screen, ship, bullets)
+
+
+run_game()
---
+++ settings.py
@@ -0,0 +1,16 @@
+class Settings:
+	"""Class for store settings of the game"""
+
+	def __init__(self):
+		"""Init game settings"""
+		# Screen settings
+		self.screen_width = 800
+		self.screen_height = 600
+		self.bg_color = (230, 230, 230)
+		# Ship settings
+		self.ship_speed_factor = 1.5
+		# Bullet settings
+		self.bullet_speed_factor = 1
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = 60, 60, 60
