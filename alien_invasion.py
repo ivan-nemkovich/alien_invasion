eeef73a6ffb8e1927dd5917c55aa5fa4aa4c06a4
Ivan Nemkovich <inemkovich@gmail.com>
2019-12-19 12:49:33 +0300

    Game ending added

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,9 +35,10 @@ def run_game():
 		clock.tick(ai_settings.fps)
 		# Mouse and keyboard detect
 		gf.check_events(ai_settings, screen, ship, bullets)
-		ship.update()
-		gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-		gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+		if stats.game_active:
+			ship.update()
+			gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+			gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
--- game_stats.py
+++ game_stats.py
@@ -5,6 +5,7 @@ class GameStats():
         """"""
         self.ai_settings = ai_settings
         self.reset_stats()
+        self.game_active = True
 
     def reset_stats(self):
         """"""
